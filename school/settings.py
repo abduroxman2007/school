7d427cf5d7848a481f19fcfa8fedc3459a3d3d94
abduroxman2007 <abduraxmonovabduraxmon794@gmail.com>
2024-11-02 11:40:15 +0500

    nimadir

--- school/settings.py
+++ school/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3oh(txph$newxw7l44pts#zksm*x16mb*ds=1g=s&9j65pp18z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
